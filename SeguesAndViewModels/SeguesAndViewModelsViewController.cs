5c7f7c023d693ce72e556ee8843e988eb98551bc
Jacob Foshee <jacobf@gmail.com>
2013-09-27 15:59:00 -0500

    New iPad Storyboard, deleted view controller boilerplate

---
+++ SeguesAndViewModels/SeguesAndViewModelsViewController.cs
@@ -0,0 +1,10 @@
+using System;
+using MonoTouch.UIKit;
+
+namespace SeguesAndViewModels
+{
+	public partial class SeguesAndViewModelsViewController : UIViewController
+	{
+		public SeguesAndViewModelsViewController(IntPtr handle) : base(handle) { }
+	}
+}
